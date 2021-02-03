4fe7ab1a440c2e91c1bb12b7219bd5a01fd8ffea
Vlad Dzinko <dzinko23@gmail.com>
2021-02-03 21:01:13 +0200

    fix void to start

--- Software/car-telemetry/Data/CANanalys.cs
+++ Software/car-telemetry/Data/CANanalys.cs
@@ -11,7 +11,7 @@ namespace car_telemetry.Data
         //"0x0F6 8 0x8E 0x87 0x32 0xFA 0x26 0x8E 0xBE 0x86"
         //перші два байти буде id, наступний байт кількість даних, далі до 8 байтів даних
         //в кінці кожного такого повідомлення буде закінчення символ нового рядка
-        public void Analyse(var string in, var string out)
+        static void Analyse(string outValue, string inValue)
         {
 
         }
