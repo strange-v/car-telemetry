29fdbf482fdc9057e8c9726311303c48b039fd80
Anbashman <34307328+Anbashman@users.noreply.github.com>
2021-02-10 20:55:33 +0200

    Create a mock for SerialService

---
+++ Hardware/Test-for-software/SerialTest.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("77E 05 62 22 0D 55 65 AA AA");//all doors close
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 00 65 AA AA");//all doors open
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 54 65 AA AA");//driver door open
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 51 65 AA AA");//passenger door open
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 50 65 AA AA");//driver and passenger door open
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 45 65 AA AA");//back left door open
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 40 65 AA AA");//driver and passenger and left back door open 
+  delay(1000);
+  Serial.println("77E 05 62 22 0D 44 65 AA AA");//driver and back right door open
+  delay(1000);
+  Serial.println("");//back right door open
+  delay(1000);
+}
